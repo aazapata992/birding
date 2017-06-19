6a8255af90e8cd6d2fead89b63197f12622d292b
aazapata <aazapata@smdigital.com.co>
2017-06-18 19:49:30 -0500

    Ajuste

--- wp-content/themes/birding/archive.php
+++ wp-content/themes/birding/archive.php
@@ -1,74 +1,29 @@
 <?php get_header(); ?>
+<section class="banner-anexo full">
+    <img src="<?php echo get_template_directory_uri(); ?>/library/images/banner-anexo.png" alt="">
+    <h1 class="title-ppal">Anexos</h1>
+</section>
+<section class="cont-anexo full">
+    <div class="wrapper-main center">
+        <?php $args = array( 'post_type' => 'birding_anexo', 'posts_per_page' => 10 );
+        $loop = new WP_Query( $args );
+        while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-			<div id="content">
-
-				<div id="inner-content" class="wrap cf">
-
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-							<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="taxonomy-description">', '</div>' );
-							?>
-							
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-
-								<header class="entry-header article-header">
-
-									<h3 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-									<p class="byline entry-meta vcard">
-										<?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                  							     /* the time the post was published */
-                  							     '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">'.__('by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-									</p>
-
-								</header>
-
-								<section class="entry-content cf">
-
-									<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
-
-									<?php the_excerpt(); ?>
-
-								</section>
-
-								<footer class="article-footer">
-
-								</footer>
-
-							</article>
-
-							<?php endwhile; ?>
-
-									<?php bones_page_navi(); ?>
-
-							<?php else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the archive.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-						</main>
-
-					<?php get_sidebar(); ?>
-
-				</div>
-
-			</div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <article class="info-anexo">
+                        <h2><?php the_title(); ?></h2>
+                        <figure>
+                            <a href="<?php the_permalink(); ?>">
+                                <img src="<?php the_post_thumbnail('full'); ?>
+                            </a>
+                        </figure>
+                        <p><?php the_content(); ?></p>
+                        <a href="<?php the_permalink(); ?>" class="mas">Conocer más</a>
+                    </article>
+                </div>
+        <?php endwhile; ?>
+    </div>
+    <div class="clr"></div>
+</section>
 
 <?php get_footer(); ?>

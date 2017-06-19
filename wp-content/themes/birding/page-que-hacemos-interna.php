9fcdf848a91f2e594df6bfe99eb8929ed879e8d2
aazapata <aazapata@smdigital.com.co>
2017-06-18 23:56:35 -0500

    Se crean los custom fields

--- wp-content/themes/birding/page-que-hacemos-interna.php
+++ wp-content/themes/birding/page-que-hacemos-interna.php
@@ -6,49 +6,35 @@
         </section>
         <section class="cont-anexo pages-que-hacemos full">
             <div class="wrapper-main center que-hacemos">
-                <article class="top-ave full">
-                    <figure>
-                        <img src="<?php echo get_template_directory_uri(); ?>/library/images/ave.png" alt="">
-                        <span>1</span>
-                    </figure>
-                    <div class="info">
-                        <h2>top endemic</h2>
-                        <p><strong>Reseña:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nemo! Rerum dicta eaque laudantium obcaecati totam eveniet, laboriosam architecto ab ut porro excepturi. Recusandae quasi consequuntur consectetur, perferendis praesentium distinctio? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, magni, perferendis saepe sed rerum quia minus. Dignissimos molestias, temporibus eius assumenda? Quas voluptatum, repudiandae inventore animi illo.</p>
-                    </div>
-                    <a href="#" class="mas">Conocer Endemic</a>
-                    <div class="clr"></div>
-                </article>
-                <article class="top-ave full">
-                    <figure>
-                        <img src="<?php echo get_template_directory_uri(); ?>/library/images/ave.png" alt="">
-                        <span>2</span>
-                    </figure>
-                    <div class="info">
-                        <h2>top endemic</h2>
-                        <p><strong>Reseña:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nemo! Rerum dicta eaque laudantium obcaecati totam eveniet, laboriosam architecto ab ut porro excepturi. Recusandae quasi consequuntur consectetur.</p>
-                    </div>
-                    <a href="#" class="mas">Conocer Endemic</a>
-                    <div class="clr"></div>
-                </article>
-                <article class="top-ave full">
-                    <figure>
-                        <img src="<?php echo get_template_directory_uri(); ?>/library/images/ave.png" alt="">
-                        <span>3</span>
-                    </figure>
-                    <div class="info">
-                        <h2>top endemic</h2>
-                        <p><strong>Reseña:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, nemo! Rerum dicta eaque laudantium obcaecati totam eveniet, laboriosam architecto ab ut porro excepturi. Recusandae quasi consequuntur consectetur, perferendis praesentium distinctio? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, magni, perferendis saepe sed rerum quia minus.</p>
-                    </div>
-                    <a href="#" class="mas">Conocer Endemic</a>
-                    <div class="clr"></div>
-                </article>
+                <?php
+                if( have_rows('ranking') ):
+                    while ( have_rows('ranking') ) : the_row(); 
+                        $imagen = get_sub_field('imagen');
+                ?>   
+                        <article class="top-ave full">
+                            <figure>
+                                <img src="<?php echo $imagen['url']; ?>" alt="<?php echo $imagen['alt'] ?>" />
+                                <span><?php the_sub_field('posicion'); ?></span>
+                            </figure>
+                            <div class="info">
+                                <h2><?php the_sub_field('nombre'); ?></h2>
+                                <p><strong>Reseña:</strong> <?php the_sub_field('resena'); ?></p>
+                            </div>
+                            <a href="<?php the_sub_field('link'); ?>" class="mas">Conocer Endemic</a>
+                            <div class="clr"></div>
+                        </article>
+
+                <?php    endwhile;
+
+                else :
+
+                    echo "No hay top que mostrar";
+
+                endif;
+                ?>
             </div>
             <div class="clr"></div>
         </section>
-
-
-
-
 
 
 <?php get_footer(); ?>
--- wp-content/themes/birding/page-quienes-somos.php
+++ wp-content/themes/birding/page-quienes-somos.php
@@ -6,91 +6,58 @@
 <section class="cont-anexo full quienes-somos">
     <div class="wrapper-main center ">
         <p class="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra, diam vitae varius suscipit, est dolor varius libero, a faucibus leo ante fringilla quam. Praesent feugiat purus a metus condimentum suscipit. Maecenas nec quam est. Mauris auctor lectus nec est sagittis, ac lobortis lectus ullamcorper. </p>
-        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-            <article class="info-anexo">
-                <h2>Guias</h2>
-                <figure>
-                    <a href="#">
-                        <img src="<?php echo get_template_directory_uri(); ?>/library/images/anexo-guias.png" alt="">
-                    </a>
-                </figure>
-                <p>Our guides are people that know the value of nature and have been engaged in conservational and educational programs raising awareness in the community. They are kind people willing to make your Birding experience in Colombia a high-quality one. Their keen eye for birds will help you to get the most out of this journey and their outstanding personalities will make your visit unforgettable.</p>
-                <a href="#" class="mas">Conocer más</a>
-            </article>
-        </div>
-        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-            <article class="info-anexo">
-                <h2>Aliados</h2>
-                <figure>
-                    <a href="#">
-                        <img src="<?php echo get_template_directory_uri(); ?>/library/images/anexox-2.png" alt="">
-                    </a>
-                </figure>
-                <p>Majestic Endemic trip texto comercial para dar un adelanto del contenido interno. Lorem ipsum in dolor sit , lorem in sit sit dolor lorem si tistis , dolor hey.</p>
-                <a href="#" class="mas">Conocer más</a>
-            </article>
-        </div>
-        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-            <article class="info-anexo">
-                <h2>Contacto</h2>
-                <figure>
-                    <a href="#">
-                        <img src="<?php echo get_template_directory_uri(); ?>/library/images/anexox-3.png" alt="">
-                    </a>
-                </figure>
-                <p>Mail de contacto: <a href="mailto:Info@Birdingforliving.com">Info@Birdingforliving.com</a></p>
-                <ul>                        
-                    <li> Name</li>
-                    <li> Last Name</li>
-                    <li> Phone Number</li>
-                    <li> Email</li>
-                    <li> Country</li>
-                    <li> Please leave us your comments </li>
-                </ul>
-                <a href="#" class="mas">Conocer más</a>
-            </article>
-        </div>
+        <?php
+            if( have_rows('items') ):
+                while ( have_rows('items') ) : the_row(); 
+                $imagen = get_sub_field('imagen');
+        ?>   
+                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                        <article class="info-anexo">
+                            <h2><?php the_sub_field('titulo'); ?></h2>
+                            <figure>
+                                <a href="<?php the_sub_field('link'); ?>">
+                                    <img src="<?php echo $imagen['url']; ?>" alt="<?php echo $imagen['alt'] ?>" />                                    
+                                </a>
+                            </figure>
+                            <p><?php the_sub_field('descripcion'); ?></p>
+                            <a href="<?php the_sub_field('link'); ?>" class="mas">Conocer más</a>
+                        </article>
+                    </div>
+        <?php    endwhile;
+                else :
+                    echo "No hay top que mostrar";
+            endif;
+        ?>
+
         <figure class="guias-quienes">
             <img src="<?php echo get_template_directory_uri(); ?>/library/images/quienes-somos.png" alt="">
         </figure>
+
+
         <div class="nuestro-equipo">
             <h3>Nuestro Equipo / Our Team</h3>
-            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                <article class="info-anexo">
-                    <h2>Arley Leandro Vargas Marín</h2>
-                    <figure>
-                        <a href="#">
-                            <img src="<?php echo get_template_directory_uri(); ?>/library/images/avatar.png" alt="">
-                        </a>
-                    </figure>
-                    <p>Avid Birdwatcher, has participated as a field observer in some investigations of diurn and nocturn birds of prey, helping to their conservation in their local communities. One of the best local guides in the municipality of “El Jardín” and with experience in humid forest, wet forest and lower montane forest of the Choco Region Reserve (Las Tangaras) and other Colombian locations such as Choco Bioregion and the Central Andes.  </p>
-                    <a href="#" class="mas">Conocer más</a>
-                </article>
-            </div>
-            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                <article class="info-anexo">
-                    <h2>Alejandro Cartagena Ramírez </h2>
-                    <figure>
-                        <a href="#">
-                            <img src="<?php echo get_template_directory_uri(); ?>/library/images/avatar.png" alt="">
-                        </a>
-                    </figure>
-                    <p>Pursuing birds since he was a kid, Alejandro decided to began exploring Colombia an its magnifiscent hotspots, exploring places such as the Choco region, Amazonian region, Central and Western Andes. Formerly a Science and English teacher, he has dedicated his life to raise awareness in schools conduncting projects that protect wildlife. Alejandro has also worked as a private cultural guide around the city of Medellin wich gives him the experience and care needed with clients of all places.</p>
-                    <a href="#" class="mas">Conocer más</a>
-                </article>
-            </div>
-            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                <article class="info-anexo">
-                    <h2>Guillermo Gutiérrez</h2>
-                    <figure>
-                        <a href="#">
-                            <img src="<?php echo get_template_directory_uri(); ?>/library/images/avatar.png" alt="">
-                        </a>
-                    </figure>
-                    <p>He is Guillermo, he has been working as a cultural and bird guide in the Bird Lodge La Esperanza. Guiding is his main activity. Birding is an important part of his life which he constantly develops around his town. His main experience has been in “El Jardín” place that he knows very well. Guillermo is well kown because of his keen ear for birdsongs. He is been working with birds that are threatened reporting their nests, behaviors and habitats to show the community the importance of the place they live in. </p>                            
-                    <a href="#" class="mas">Conocer más</a>
-                </article>
-            </div>
+            <?php
+                if( have_rows('nuestro_equipo') ):
+                    while ( have_rows('nuestro_equipo') ) : the_row(); 
+                    $imagen2 = get_sub_field('foto');
+            ?>   
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                            <article class="info-anexo">
+                                <h2><?php the_sub_field('nombre'); ?></h2>
+                                <figure>
+                                    <a href="<?php the_sub_field('link'); ?>">
+                                        <img src="<?php echo $imagen2['url']; ?>" alt="<?php echo $imagen2['alt'] ?>" />                                    
+                                    </a>
+                                </figure>
+                                <p><?php the_sub_field('descripcion'); ?></p>
+                                <a href="<?php the_sub_field('link'); ?>" class="mas">Conocer más</a>
+                            </article>
+                        </div>
+            <?php    endwhile;
+                    else :
+                        echo "No hay top que mostrar";
+                endif;
+            ?>
         </div>
     </div>
     <div class="bg-aliados full">
@@ -137,9 +104,5 @@
     </div>
     <div class="clr"></div>
 </section>
-
-
-
-
 
 <?php get_footer(); ?>

d8a52167cc7a7c6406cf28b22854902f54290c36
aazapata <aazapata@smdigital.com.co>
2017-06-20 08:22:04 -0500

    Se ajustan internas

--- wp-content/themes/birding/page-ruta-magestic.php
+++ wp-content/themes/birding/page-ruta-magestic.php
@@ -28,7 +28,7 @@
 
 <section class="banner-anexo full">
     <img src="<?php echo get_template_directory_uri() ?>/library/images/banner-anexo.png" alt="">
-    <h1 class="title-ppal">Qué Hacemos?</h1>            
+    <h1 class="title-ppal"><?php the_title(); ?></h1>            
 </section>
 <div class="cont-galeria-hacemos full">
     <div class="wrapper-main center full cont-contacto">
@@ -125,7 +125,7 @@
                                     <?php if ($route['pin'] === "1"): ?>
                                         <div class="total">
                                             <span>
-                                                <img src="<?php echo get_template_directory_uri() ?>/library/images/route-magestic/bird-white.png" alt="">
+                                                <img src="<?php echo get_template_directory_uri() ?>/library/images/icon-ave.png" alt="">
                                             </span>
                                             Total de Aves: <span id="route-count"><?php echo $route['count'] ?></span>
                                         </div>
@@ -140,4 +140,86 @@
     </div>
     <div class="clr"></div>
 </div>
+
+<section class="full" style="background: #eeeeee;">
+    <div class="wrapper-main center">
+        <div class="cont-info-banner">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <article class="info-top-home" style="max-width: 100%;">
+                    <div class="bg" style="width: 100%;">
+                        <h3><?php the_field('titulo'); ?></h3>
+                        <h2><?php the_field('subtitulo'); ?></h2>
+                        <figure>
+                            <?php $image = get_field('imagen'); ?>
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                        </figure>
+
+                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                            <div class="height-marco1">
+                                <p><?php the_field('descripcion'); ?></p>
+
+                                <p style="font-family: arial;font-style: italic;"><img src="<?php echo get_template_directory_uri() ?>/library/images/1.jpg" alt=""> <?php the_field('altura_clima'); ?></p>
+                                <p style="font-family: arial;font-style: italic;"><img src="<?php echo get_template_directory_uri() ?>/library/images/2.jpg" alt=""> <?php the_field('vestuario'); ?></p>
+                                <p style="font-family: arial;font-style: italic;"><img src="<?php echo get_template_directory_uri() ?>/library/images/3.jpg" alt=""> <?php the_field('condiciones_medicas'); ?></p>
+                                <p style="font-family: arial;font-style: italic;"><img src="<?php echo get_template_directory_uri() ?>/library/images/4.jpg" alt=""> <?php the_field('nivel_dificultad'); ?></p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        </div>
+                        <!-- <a href="#" class="btns comprar">Comprar</a> -->
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
+<br>
+<br>
+<br>
+<section class="guias-home full">
+    <div class="wrapper-main center">
+        <figure>
+            <h2>Guias</h2>
+            <img src="<?php echo get_template_directory_uri(); ?>/library/images/quienes-somos.png" alt="">
+        </figure>
+        <a href="#" class="btn-red">Conocer más</a>
+    </div>
+</section>
+<br>
+<br>
+<br>
+<section class="bg-contacto-home">
+    <div class="wrapper-main center full cont-contacto">
+        <h2>Contacto</h2>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <div class="campos">
+                <label>Name</label>
+                <input type="text">
+            </div>
+            <div class="campos">
+                <label>Last Name</label>
+                <input type="text">
+            </div>
+            <div class="campos">
+                <label>Phone Number</label>
+                <input type="text">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <div class="campos">
+                <label>Email</label>
+                <input type="email">
+            </div>
+            <div class="campos">
+                <label>Country</label>
+                <input type="text">
+            </div>
+            <div class="campos">
+                <label>Please leave us your comments</label>
+                <textarea></textarea>
+            </div>
+            <input type="submit" value="SEND">
+        </div>
+    </div>
+</section>
 <?php get_footer() ?>
